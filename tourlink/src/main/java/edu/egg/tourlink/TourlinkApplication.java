913c5ead6c0ec9cade2a74e4543d9fae695e9469
mayrarosas <58344716+mayrarosas@users.noreply.github.com>
2019-12-06 18:43:52 -0300

    Add files via upload
    Primer upload cargando 16 de 17

---
+++ tourlink/src/main/java/edu/egg/tourlink/TourlinkApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.tourlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourlinkApplication.class, args);
+	}
+
+}
---
+++ tourlink/src/test/java/edu/egg/tourlink/TourlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.egg.tourlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
