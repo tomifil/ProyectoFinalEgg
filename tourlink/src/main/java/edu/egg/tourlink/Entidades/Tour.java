3f120c2168d22de81fdbb4ce358294b3a3824b69
JuliKM <57647619+JuliKM@users.noreply.github.com>
2019-12-09 18:52:41 -0300

    Update Tour.java

--- tourlink/src/main/java/edu/egg/tourlink/Entidades/Tour.java
+++ tourlink/src/main/java/edu/egg/tourlink/Entidades/Tour.java
@@ -1,10 +1,15 @@
-
 package edu.egg.tourlink.Entidades;
 
+import edu.egg.tourlink.Enumeraciones.Tipo_tour;
 import java.util.Date;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -12,16 +17,28 @@ public class Tour {
 
     @Id
     @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid" , strategy = "uuid2" )
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private int id;
+
+    @Temporal(TemporalType.TIMESTAMP)
     private Date fecha;
     private String horario;
 
+    @Enumerated(EnumType.STRING)
+    private Tipo_tour tipo_tour;
+    
+    
     public Tour(int id, Date fecha, String horario) {
         this.id = id;
         this.fecha = fecha;
         this.horario = horario;
     }
+    @OneToMany
+    private Idioma idioma;
+    
+    @OneToMany
+    private Calificacion calificacion;
+    
 
     public Tour() {
     }
@@ -50,7 +67,4 @@ public class Tour {
         this.horario = horario;
     }
 
-    
-    
-    
 }
