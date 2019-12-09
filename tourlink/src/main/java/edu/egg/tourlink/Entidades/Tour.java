1a680df5627f82994bdc6b606765f1c7d0466b30
JuliKM <57647619+JuliKM@users.noreply.github.com>
2019-12-09 18:46:25 -0300

    Clase tour
    Arme los atributos de clase Tour

--- tourlink/src/main/java/edu/egg/tourlink/Entidades/Tour.java
+++ tourlink/src/main/java/edu/egg/tourlink/Entidades/Tour.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.egg.tourlink.Entidades;
 
-/**
- *
- * @author DELL
- */
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Tour {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid" , strategy = "uuid2" )
+    private int id;
+    private Date fecha;
+    private String horario;
+
+    public Tour(int id, Date fecha, String horario) {
+        this.id = id;
+        this.fecha = fecha;
+        this.horario = horario;
+    }
+
+    public Tour() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    
+    
     
 }
