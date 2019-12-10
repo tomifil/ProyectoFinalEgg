5b8d922fd574d115ff4518762c5c7a2f6033a705
JuliKM <57647619+JuliKM@users.noreply.github.com>
2019-12-10 18:44:32 -0300

    Update TourRepositorio

--- tourlink/src/main/java/edu/egg/tourlink/Repositorios/TourRepositorio.java
+++ tourlink/src/main/java/edu/egg/tourlink/Repositorios/TourRepositorio.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.egg.tourlink.Repositorios;
 
-/**
- *
- * @author DELL
- */
-public class TourRepositorio {
+import edu.egg.tourlink.Entidades.Tour;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TourRepositorio extends JpaRepository<Tour, String>{
     
+      @Query("Select c FROM Tour c WHERE c.tour.id = :id")
+    public List<Tour> buscarTourPorId(@Param("id") String id);
 }
