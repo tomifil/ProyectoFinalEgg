a1f210b9d3b5a53b7ce2d3f18efcaa0f669b8333
LuBecerra <LuBecerra_2019@users.noreply.github.com>
2019-12-10 19:11:41 -0300

    Create Foto.java

---
+++ tourlink/src/main/java/edu/egg/tourlink/Entidades/Foto.java
@@ -0,0 +1,78 @@
+
+package edu.egg.tourlink.Entidades;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+
+@Entity
+public class Foto {
+    @Id
+    private String id;
+    private String nombre;
+    private String mime;
+    @Lob @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the mime
+     */
+    public String getMime() {
+        return mime;
+    }
+
+    /**
+     * @param mime the mime to set
+     */
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    /**
+     * @return the contenido
+     */
+    public byte[] getContenido() {
+        return contenido;
+    }
+
+    /**
+     * @param contenido the contenido to set
+     */
+    public void setContenido(byte[] contenido) {
+        this.contenido = contenido;
+    }
+    
+    
+}
+
