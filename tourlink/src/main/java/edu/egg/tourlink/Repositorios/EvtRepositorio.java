5b6c7e8675750666081a9d24196cc99aabb2779a
LuBecerra <LuBecerra_2019@users.noreply.github.com>
2019-12-10 18:50:04 -0300

    Repositorio EVT

---
+++ tourlink/src/main/java/edu/egg/tourlink/Repositorios/EvtRepositorio.java
@@ -0,0 +1,14 @@
+
+package edu.egg.tourlink.Repositorios;
+
+import edu.egg.tourlink.Entidades.EVT;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EvtRepositorio extends JpaRepository<EVT, String>{
+    @Query("SELECT c FROM EVT c WHERE c.legajo_id = :legajo_id")
+    public EVT buscarPorLegajo(@Param("legajo_id")int legajo_id);
+}
