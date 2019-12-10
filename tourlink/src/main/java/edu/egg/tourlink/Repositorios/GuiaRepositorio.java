73112935dd930909123588b3b8f86082756dffba
Mayra <mayrarosas804@gmail.com>
2019-12-10 18:58:27 -0300

    Update GuiaRepositorio.java

--- tourlink/src/main/java/edu/egg/tourlink/Repositorios/GuiaRepositorio.java
+++ tourlink/src/main/java/edu/egg/tourlink/Repositorios/GuiaRepositorio.java
@@ -1,6 +1,15 @@
 
 package edu.egg.tourlink.Repositorios;
 
-public class GuiaRepositorio {
-    
+import edu.egg.tourlink.Entidades.Guia;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuiaRepositorio extends JpaRepository<Guia, Long> {
+   @Query( "SELECT c FROM Guia c WHERE c.dni = :dni ") 
+   public List<Guia> buscarPorGuia (@Param("id") String id);
 }
