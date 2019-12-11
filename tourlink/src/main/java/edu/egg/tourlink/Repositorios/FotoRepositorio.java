bdda011265ef966c21eda051b0b08f16a243968f
oyolatomas <58391496+oyolatomas@users.noreply.github.com>
2019-12-11 19:35:09 -0300

    e

--- tourlink/src/main/java/edu/egg/tourlink/Repositorios/FotoRepositorio.java
+++ tourlink/src/main/java/edu/egg/tourlink/Repositorios/FotoRepositorio.java
@@ -3,7 +3,9 @@ package edu.egg.tourlink.Repositorios;
 
 import edu.egg.tourlink.Entidades.Foto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FotoRepositorio extends JpaRepository<Foto,String> {
     
 }
--- tourlink/src/main/java/edu/egg/tourlink/Servicios/TourServicio.java
+++ tourlink/src/main/java/edu/egg/tourlink/Servicios/TourServicio.java
@@ -14,7 +14,9 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TourServicio {
     @Autowired
     private EvtRepositorio evtRepositorio;
