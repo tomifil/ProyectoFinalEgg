6b1dbfeb3b20df2cc6f7501c47dc99fe83dc9e02
LuBecerra <LuBecerra_2019@users.noreply.github.com>
2019-12-11 19:37:51 -0300

    lala

--- tourlink/src/main/java/edu/egg/tourlink/TourlinkApplication.java
+++ tourlink/src/main/java/edu/egg/tourlink/TourlinkApplication.java
@@ -1,5 +1,4 @@
 package edu.egg.tourlink;
-//holi c:
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +9,3 @@ public class TourlinkApplication {
 		SpringApplication.run(TourlinkApplication.class, args);
 	}
 }
-// hola
