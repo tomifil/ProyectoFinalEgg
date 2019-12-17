a7e653d6aaf1a844c773c06b9cd0180814b2c987
LuBecerra <LuBecerra_2019@users.noreply.github.com>
2019-12-17 18:23:41 -0300

    update html

--- tourlink/src/main/java/edu/egg/tourlink/Controladores/PortalControlador.java
+++ tourlink/src/main/java/edu/egg/tourlink/Controladores/PortalControlador.java
@@ -43,7 +43,10 @@ public class PortalControlador {
     public String guias() {
         return "verGuias.html";
     }
-    
+    @GetMapping("/editarEvt")
+    public String editarEvt() {
+        return "editarEvt.html";
+    }
 
 //    Registro de GUÍA
     @Autowired
