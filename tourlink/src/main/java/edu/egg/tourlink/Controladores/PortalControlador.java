8b1c51ba7444bb6b0ba27c16ec70d50ff14cbb08
LuBecerra <LuBecerra_2019@users.noreply.github.com>
2019-12-18 17:40:15 -0300

    Update PortalControlador.java

--- tourlink/src/main/java/edu/egg/tourlink/Controladores/PortalControlador.java
+++ tourlink/src/main/java/edu/egg/tourlink/Controladores/PortalControlador.java
@@ -1,13 +1,12 @@
-
 package edu.egg.tourlink.Controladores;
 
 import edu.egg.tourlink.Entidades.EVT;
 import edu.egg.tourlink.Entidades.Guia;
 import edu.egg.tourlink.Entidades.Idioma;
+import edu.egg.tourlink.Enumeraciones.Tipo_idioma;
 import edu.egg.tourlink.Errores.ErrorServicio;
 import edu.egg.tourlink.Repositorios.EvtRepositorio;
 import edu.egg.tourlink.Repositorios.GuiaRepositorio;
-import edu.egg.tourlink.Repositorios.IdiomaRepositorio;
 import edu.egg.tourlink.Repositorios.TipoTourRepositorio;
 import edu.egg.tourlink.Repositorios.TourRepositorio;
 import edu.egg.tourlink.Repositorios.UsuarioRepositorio;
@@ -18,13 +17,11 @@ import edu.egg.tourlink.entidades.Tipo_tour;
 import edu.egg.tourlink.entidades.Usuario;
 import edu.egg.tourlink.enumeraciones.Rol;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 
 
 import java.util.Date;
 
 import java.util.List;
-import javax.mail.internet.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -38,7 +35,6 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    private Object dateFormat;
 
     @GetMapping({"/", "/home"})
     public String index() {
@@ -83,9 +79,6 @@ public class PortalControlador {
 
     @Autowired
     TipoTourRepositorio tipotourRepositorio;
-    
-    @Autowired
-    IdiomaRepositorio idiomaRepositorio;
     
     @PostMapping("/crearGuia")
     public String crearGuia(@RequestParam(required = false, value = "imagen") MultipartFile archivo, @RequestParam(value = "dni") long dni, @RequestParam(value = "nombre") String nombre, @RequestParam(value = "apellido") String apellido,
@@ -177,18 +170,10 @@ public class PortalControlador {
 
     
         @PostMapping("/crearTour")
-    public String crearTour(@RequestParam(value = "legajo_id") String legajo_id, @RequestParam(value = "tipo_tour") String nombre_tipo_tour, @RequestParam(value = "idioma") String nombre_idioma, @RequestParam(value = "fecha") String fecha, @RequestParam(value = "horario") String horario) throws ErrorServicio, java.text.ParseException {
-        Date date1 = null;
-        System.out.println(fecha);
-        date1 = new SimpleDateFormat("yyyy-MM-dd").parse(fecha);
+    public String crearTour(@RequestParam(value = "legajo_id") String legajo_id, @RequestParam(value = "tipo_tour") Tipo_tour tipo_tour, @RequestParam(value = "idioma") Tipo_idioma tipo_idioma, /List<Calificacion> calificaciones,/ @RequestParam(value = "fecha") Date fecha, @RequestParam(value = "horario") String horario) throws ErrorServicio {
+
         try {
-            Tipo_tour tipo_tour = new Tipo_tour();
-            tipo_tour.setNombre(tipotourRepositorio.buscarPorNombre(nombre_tipo_tour));
-            
-            Idioma idioma = new Idioma();
-            idioma.setId(idiomaRepositorio.buscarPorNombre(nombre_idioma));
-            
-            tourServicio.agregarTour(legajo_id, tipo_tour, idioma, date1, horario);
+            tourServicio.agregarTour(legajo_id, tipo_tour, tipo_idioma, fecha, horario);
         } catch (ErrorServicio e) {
             e.printStackTrace();
             System.out.println("Faltan datos");
@@ -197,21 +182,17 @@ public class PortalControlador {
     }
 
     @PostMapping("/modificarTour")
-    public String modificarTour(@RequestParam(value = "id") String id, @RequestParam(value = "legajo_id") String legajo_id, @RequestParam(value = "tipo_tour") String nombre_tipo_tour, @RequestParam(value = "idioma") String idioma, /*List<Calificacion> calificaciones,*/ @RequestParam(value = "fecha") Date fecha, @RequestParam(value = "horario") String horario) throws ErrorServicio {
+    public String modificarTour(@RequestParam(value = "id") String id, @RequestParam(value = "legajo_id") String legajo_id, @RequestParam(value = "tipo_tour") Tipo_tour tipo_tour, @RequestParam(value = "idioma") Tipo_idioma tipo_idioma, /List<Calificacion> calificaciones,/ @RequestParam(value = "fecha") Date fecha, @RequestParam(value = "horario") String horario) throws ErrorServicio {
 
-//        if (tourRepositorio.findById(id) != null) {
-//            try {
-//               
-//                
-//                
-//                    
-//                tourServicio.modificarTour(legajo_id, id, tipo_tour, idioma, fecha, horario);
-//            } catch (ErrorServicio e) {
-//                e.printStackTrace();
-//                System.out.println("Faltan datos");
-//
-//            }
-//        }
+        if (tourRepositorio.findById(id) != null) {
+            try {
+                tourServicio.modificarTour(legajo_id, id, tipo_tour, tipo_idioma, fecha, horario);
+            } catch (ErrorServicio e) {
+                e.printStackTrace();
+                System.out.println("Faltan datos");
+
+            }
+        }
         return "editarEvt.html";
     }
    
